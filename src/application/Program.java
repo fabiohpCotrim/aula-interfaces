7aa629c1ebaa0a5e726d4f73310c6d67d737330c
Fabio Cotrim <fabiohenrique_p13@hotmail.com>
2023-05-31 17:00:26 -0300

    Entities package and classes created

---
+++ src/application/Program.java
@@ -0,0 +1,34 @@
+package application;
+
+import java.time.LocalDateTime;
+import java.time.format.DateTimeFormatter;
+import java.util.Locale;
+import java.util.Scanner;
+
+import model.entities.CarRental;
+import model.entities.Vehicle;
+
+public class Program {
+
+	public static void main(String[] args) {
+
+		Locale.setDefault(Locale.US);
+		Scanner sc = new Scanner(System.in);
+		
+		System.out.println("Enter rental data: ");
+		System.out.print("Car model: ");
+		String carModel = sc.nextLine();
+		System.out.print("Pickup (dd/MM/yyyy HH:mm): ");
+		LocalDateTime start = LocalDateTime.parse(sc.nextLine(), DateTimeFormatter.ofPattern("dd/MM/yyyy HH:mm"));
+		System.out.print("Return (dd/MM/yyyy HH:mm): ");
+		LocalDateTime finish = LocalDateTime.parse(sc.nextLine(), DateTimeFormatter.ofPattern("dd/MM/yyyy HH:mm"));
+	
+		CarRental cr = new CarRental(start, finish, new Vehicle(carModel));
+		
+		
+		
+		sc.close();
+
+	}
+
+}
